22c274ddd8d32cd61c62b454d373052d637b7f4d
Anandhu336 <0madaa90@solent.ac.uk>
2026-02-21 08:01:38 +0000

    Update database.py

--- Backend/db/database.py
+++ Backend/db/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 
-DATABASE_URL = "postgresql://warehouse_user:warehouse123@localhost/warehouse_db"
+DATABASE_URL = "postgresql://warehouse_wlem_user:8lqVuXJSW1art6isywgI3iTgYsGygjq1@dpg-d6cma8ogjchc739o0mlg-a/warehouse_wlem"
 
 engine = create_engine(DATABASE_URL)
